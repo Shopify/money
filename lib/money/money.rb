86024e50b7b1c38791d9f31fba25812aa8befd3d
David Cornu <davidjcornu@gmail.com>
2014-03-28 18:21:23 +0000

    Call .to_money before .cents when comparing

--- lib/money/money.rb
+++ lib/money/money.rb
@@ -3,20 +3,20 @@ require 'bigdecimal/util'
 
 class Money
   include Comparable
-  
+
   attr_reader :value, :cents
 
   def initialize(value = 0)
     raise ArgumentError if value.respond_to?(:nan?) && value.nan?
-    
+
     @value = value_to_decimal(value).round(2)
     @cents = (@value * 100).to_i
   end
-  
+
   def <=>(other)
-    cents <=> other.cents
+    cents <=> other.to_money.cents
   end
-  
+
   def +(other)
     Money.new(value + other.to_money.value)
   end
@@ -24,23 +24,23 @@ class Money
   def -(other)
     Money.new(value - other.to_money.value)
   end
-  
+
   def *(numeric)
     Money.new(value * numeric)
   end
-  
+
   def /(numeric)
     raise "[Money] Dividing money objects can lose pennies. Use #split instead"
   end
-    
+
   def inspect
     "#<#{self.class} value:#{self.to_s}>"
   end
-  
+
   def ==(other)
     eql?(other)
   end
-  
+
   def eql?(other)
     self.class == other.class && value == other.value
   end
@@ -53,49 +53,49 @@ class Money
   def hash
     value.hash
   end
-  
+
   def self.parse(input)
     parser.parse(input)
   end
-  
+
   # allow parser to be set via dependency injection.
   def self.parser
     @@parser ||= MoneyParser
   end
-  
+
   def self.parser=(new_parser_class)
     @@parser = new_parser_class
   end
-  
+
   def self.empty
     Money.new
   end
-  
+
   def self.from_cents(cents)
     Money.new(cents.round.to_f / 100)
   end
-  
+
   def to_money
     self
   end
-  
+
   def zero?
     value.zero?
   end
-  
+
   # dangerous, this *will* shave off all your cents
   def to_i
     value.to_i
   end
-  
+
   def to_f
     value.to_f
   end
-  
+
   def to_s
     sprintf("%.2f", value.to_f)
   end
-  
+
   def to_liquid
     cents
   end
@@ -103,11 +103,11 @@ class Money
   def to_json(options = {})
     to_s
   end
-  
+
   def as_json(*args)
     to_s
   end
-  
+
   def abs
     Money.new(value.abs)
   end
@@ -122,25 +122,25 @@ class Money
     result = value / (1 + rate)
     Money.new(result)
   end
-  
-  # Allocates money between different parties without losing pennies. 
+
+  # Allocates money between different parties without losing pennies.
   # After the mathmatically split has been performed, left over pennies will
   # be distributed round-robin amongst the parties. This means that parties
   # listed first will likely recieve more pennies then ones that are listed later
-  # 
+  #
   # @param [0.50, 0.25, 0.25] to give 50% of the cash to party1, 25% ot party2, and 25% to party3.
   #
   # @return [Array<Money, Money, Money>]
   #
   # @example
-  #   Money.new(5, "USD").allocate([0.3,0.7)) #=> [Money.new(2), Money.new(3)]  
+  #   Money.new(5, "USD").allocate([0.3,0.7)) #=> [Money.new(2), Money.new(3)]
   #   Money.new(100, "USD").allocate([0.33,0.33,0.33]) #=> [Money.new(34), Money.new(33), Money.new(33)]
   def allocate(splits)
     allocations = splits.inject(0.0) {|sum, i| sum += i }
     raise ArgumentError, "splits add to more then 100%" if allocations > 1.0
 
     left_over = cents
- 
+
     amounts = splits.collect do |ratio|
       fraction = (cents * ratio / allocations).floor
       left_over -= fraction
@@ -190,4 +190,3 @@ class Money
     end
   end
 end
-
--- spec/money_spec.rb
+++ spec/money_spec.rb
@@ -1,111 +1,111 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "Money" do
-  
+
   before(:each) do
     @money = Money.new
   end
-  
+
   it "should be contructable with empty class method" do
     Money.empty.should == @money
   end
-  
+
   it "should return itself with to_money" do
     @money.to_money.should equal(@money)
   end
-  
+
   it "should default to 0 when constructed with no arguments" do
     @money.should == Money.new(0.00)
   end
-  
+
   it "should to_s as a float with 2 decimal places" do
     @money.to_s.should == "0.00"
   end
-  
+
   it "should be constructable with a BigDecimal" do
     Money.new(BigDecimal.new("1.23")).should == Money.new(1.23)
   end
-  
+
   it "should be constructable with a Fixnum" do
     Money.new(3).should == Money.new(3.00)
   end
-  
+
   it "should be construcatable with a Float" do
     Money.new(3.00).should == Money.new(3.00)
   end
-  
+
   it "should be addable" do
     (Money.new(1.51) + Money.new(3.49)).should == Money.new(5.00)
   end
-  
+
   it "should be able to add $0 + $0" do
     (Money.new + Money.new).should == Money.new
   end
-  
+
   it "should be subtractable" do
     (Money.new(5.00) - Money.new(3.49)).should == Money.new(1.51)
   end
-  
+
   it "should be subtractable to $0" do
     (Money.new(5.00) - Money.new(5.00)).should == Money.new
   end
-  
+
   it "should be substractable to a negative amount" do
     (Money.new(0.00) - Money.new(1.00)).should == Money.new("-1.00")
   end
-  
+
   it "should inspect to a presentable string" do
     @money.inspect.should == "#<Money value:0.00>"
   end
-  
+
   it "should be inspectable within an array" do
     [@money].inspect.should == "[#<Money value:0.00>]"
   end
-  
+
   it "should correctly support eql? as a value object" do
     @money.should eql(Money.new)
   end
-  
+
   it "should be addable with integer" do
     (Money.new(1.33) + 1).should == Money.new(2.33)
     (1 + Money.new(1.33)).should == Money.new(2.33)
   end
-  
+
   it "should be addable with float" do
     (Money.new(1.33) + 1.50).should == Money.new(2.83)
     (1.50 + Money.new(1.33)).should == Money.new(2.83)
   end
-  
+
   it "should be multipliable with an integer" do
     (Money.new(1.00) * 55).should == Money.new(55.00)
     (55 * Money.new(1.00)).should == Money.new(55.00)
   end
-  
+
   it "should be multiplable with a float" do
     (Money.new(1.00) * 1.50).should == Money.new(1.50)
     (1.50 * Money.new(1.00)).should == Money.new(1.50)
   end
-  
+
   it "should be multipliable by a cents amount" do
     (Money.new(1.00) * 0.50).should == Money.new(0.50)
     (0.50 * Money.new(1.00)).should == Money.new(0.50)
   end
-  
+
   it "should be multipliable by a repeatable floating point number" do
     (Money.new(24.00) * (1 / 30.0)).should == Money.new(0.80)
     ((1 / 30.0) * Money.new(24.00)).should == Money.new(0.80)
   end
-  
+
   it "should round multiplication result with fractional penny of 5 or higher up" do
     (Money.new(0.03) * 0.5).should == Money.new(0.02)
     (0.5 * Money.new(0.03)).should == Money.new(0.02)
   end
-  
+
   it "should round multiplication result with fractional penny of 4 or lower down" do
     (Money.new(0.10) * 0.33).should == Money.new(0.03)
     (0.33 * Money.new(0.10)).should == Money.new(0.03)
   end
-  
+
   it "should raise if divided" do
     lambda { Money.new(55.00) / 55 }.should raise_error
   end
@@ -113,39 +113,39 @@ describe "Money" do
   it "should return cents in to_liquid" do
     Money.new(1.00).to_liquid.should == 100
   end
-  
+
   it "should return cents in to_json" do
     Money.new(1.00).to_liquid.should == 100
   end
-  
+
   it "should support absolute value" do
     Money.new(-1.00).abs.should == Money.new(1.00)
   end
-  
+
   it "should support to_i" do
     Money.new(1.50).to_i.should == 1
   end
-  
+
   it "should support to_f" do
     Money.new(1.50).to_f.to_s.should == "1.5"
   end
-  
+
   it "should be creatable from an integer value in cents" do
     Money.from_cents(1950).should == Money.new(19.50)
   end
-  
+
   it "should be creatable from an integer value of 0 in cents" do
     Money.from_cents(0).should == Money.new
   end
-  
+
   it "should be creatable from a float cents amount" do
     Money.from_cents(1950.5).should == Money.new(19.51)
   end
-  
+
   it "should raise when constructed with a NaN value" do
     lambda{ Money.new( 0.0 / 0) }.should raise_error
   end
-  
+
   it "should be comparable with non-money objects" do
     @money.should_not == nil
   end
@@ -155,7 +155,7 @@ describe "Money" do
     Money.new(18.99).floor.should == Money.new(18.00)
     Money.new(21).floor.should == Money.new(21)
   end
-  
+
   describe "frozen with amount of $1" do
     before(:each) do
       @money = Money.new(1.00).freeze
@@ -164,54 +164,60 @@ describe "Money" do
     it "should == $1" do
       @money.should == Money.new(1.00)
     end
-    
+
     it "should not == $2" do
       @money.should_not == Money.new(2.00)
     end
-    
+
     it "<=> $1 should be 0" do
       (@money <=> Money.new(1.00)).should == 0
     end
-    
+
     it "<=> $2 should be -1" do
       (@money <=> Money.new(2.00)).should == -1
     end
-    
+
     it "<=> $0.50 should equal 1" do
       (@money <=> Money.new(0.50)).should == 1
     end
-    
+
+    it "<=> works with non-money objects" do
+      (@money <=> 1).should == 0
+      (@money <=> 2).should == -1
+      (@money <=> 0.5).should == 1
+    end
+
     it "should have the same hash value as $1" do
       @money.hash.should == Money.new(1.00).hash
     end
-    
+
     it "should not have the same hash value as $2" do
       @money.hash.should == Money.new(1.00).hash
     end
 
   end
-  
+
   describe "with amount of $0" do
     before(:each) do
       @money = Money.new
     end
-    
+
     it "should be zero" do
       @money.should be_zero
     end
-    
+
     it "should be greater than -$1" do
       @money.should be > Money.new("-1.00")
     end
-    
+
     it "should be greater than or equal to $0" do
       @money.should be >= Money.new
     end
-    
+
     it "should be less than or equal to $0" do
       @money.should be <= Money.new
     end
-    
+
     it "should be less than $1" do
       @money.should be < Money.new(1.00)
     end
@@ -221,31 +227,31 @@ describe "Money" do
     before(:each) do
       @money = Money.new(1.00)
     end
-    
+
     it "should not be zero" do
       @money.should_not be_zero
     end
-    
+
     it "should have a decimal value = 1.00" do
       @money.value.should == BigDecimal.new("1.00")
     end
-    
+
     it "should have 100 cents" do
       @money.cents.should == 100
     end
-    
+
     it "should return cents as a Fixnum" do
       @money.cents.should be_an_instance_of(Fixnum)
     end
-    
+
     it "should be greater than $0" do
       @money.should be > Money.new(0.00)
     end
-    
+
     it "should be less than $2" do
       @money.should be < Money.new(2.00)
     end
-    
+
     it "should be equal to $1" do
       @money.should == Money.new(1.00)
     end
@@ -255,7 +261,7 @@ describe "Money" do
     specify "#allocate takes no action when one gets all" do
       Money.new(5).allocate([1]).should == [Money.new(5)]
     end
-    
+
     specify "#allocate does not lose pennies" do
       moneys = Money.new(0.05).allocate([0.3,0.7])
       moneys[0].should == Money.new(0.02)
@@ -268,7 +274,7 @@ describe "Money" do
       moneys[1].cents.should == 33
       moneys[2].cents.should == 33
     end
-    
+
     specify "#allocate requires total to be less then 1" do
       lambda { Money.new(0.05).allocate([0.5,0.6]) }.should raise_error(ArgumentError)
     end
@@ -311,22 +317,22 @@ describe "Money" do
       Money.new(35.50).fraction(0).should == Money.new(35.50)
     end
   end
-  
+
   describe "with amount of $1 with created with 3 decimal places" do
     before(:each) do
       @money = Money.new(1.125)
     end
-    
+
     it "should round 3rd decimal place" do
       @money.value.should == BigDecimal.new("1.13")
     end
   end
-  
+
   describe "parser dependency injection" do
     before(:each) do
       Money.parser = AccountingMoneyParser
     end
-    
+
     it "should keep AccountingMoneyParser class on new money objects" do
       Money.new.class.parser.should == AccountingMoneyParser
     end
@@ -334,11 +340,11 @@ describe "Money" do
     it "should support parenthesis from AccountingMoneyParser" do
       Money.parse("($5.00)").should == Money.new(-5)
     end
-    
+
     it "should support parenthesis from AccountingMoneyParser for .to_money" do
       "($5.00)".to_money.should == Money.new(-5)
     end
-    
+
     after(:each) do
       Money.parser = nil # reset
     end
