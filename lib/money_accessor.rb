22a9feff16b7a3fa412f67626fff5c108bea4592
Maxime Bedard <maxime.bedard@shopify.com>
2015-10-26 17:07:26 +0000

    Use struct accessor instead of instance variable set/get

--- lib/money_accessor.rb
+++ lib/money_accessor.rb
@@ -7,21 +7,39 @@ module MoneyAccessor
     def money_accessor(*columns)
       Array(columns).flatten.each do |name|
         define_method(name) do
-          value = instance_variable_get("@#{name}")
+          value = _money_get(name)
           value.blank? ? nil : Money.new(value)
         end
 
         define_method("#{name}=") do |value|
           if value.blank? || !value.respond_to?(:to_money)
-            instance_variable_set("@#{name}", nil)
+            _money_set(name, nil)
             nil
           else
             money = value.to_money
-            instance_variable_set("@#{name}", money.value)
+            _money_set(name, money.value)
             money
           end
         end
       end
+    end
+  end
+
+  private
+  
+  def _money_set(ivar, value)
+    if self.is_a?(Struct)
+      self[ivar] = value
+    else
+      instance_variable_set("@#{ivar}", value)
+    end
+  end
+
+  def _money_get(ivar)
+    if self.is_a?(Struct)
+      self[ivar]
+    else
+      instance_variable_get("@#{ivar}")
     end
   end
 end
