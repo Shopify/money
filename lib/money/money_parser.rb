23faf8255b566d9ca337def263be263a60829603
Samir El Maoujoudi <samir.elmaoujoudi@gmail.com>
2017-01-17 14:30:45 +0100

    Fix parsing for digits with more than 2 decimals

--- lib/money/money_parser.rb
+++ lib/money/money_parser.rb
@@ -5,29 +5,29 @@ class MoneyParser
   def self.parse(input)
     new.parse(input)
   end
-  
+
   def parse(input)
     Money.new(extract_money(input))
   end
-  
+
   private
   def extract_money(input)
     return ZERO_MONEY if input.to_s.empty?
-    
+
     amount = input.scan(/\-?[\d\.\,]+/).first
-            
+
     return ZERO_MONEY if amount.nil?
-    
-    # Convert 0.123 or 0,123 into what will be parsed as a decimal amount 0.12 or 0.13
-    amount.gsub!(/^(-)?(0[,.]\d\d)\d+$/, '\1\2')
-            
+
+    # Convert amount with more than 3 decimals to amount with 2 decimals
+    amount.gsub!(/^(-)?(\d{1,}[,.]\d\d)[1-9]+$/, '\1\2')
+
     segments = amount.scan(/^(.*?)(?:[\.\,](\d{1,2}))?$/).first
-        
-    return ZERO_MONEY if segments.empty?    
-    
+
+    return ZERO_MONEY if segments.empty?
+
     amount   = segments[0].gsub(/[^-\d]/, '')
     decimals = segments[1].to_s.ljust(2, '0')
-    
+
     "#{amount}.#{decimals}"
   end
 end
