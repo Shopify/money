3f7db98722c836c5baf60989834a1979f96d9721
Jean Boussier <jean.boussier@gmail.com>
2017-04-27 17:34:49 +0200

    Make MoneyColumn use the new AR::Type API so that the values get cached

--- lib/money_column/active_record_hooks.rb
+++ lib/money_column/active_record_hooks.rb
@@ -1,4 +1,15 @@
 module MoneyColumn
+  class Type < ActiveRecord::Type::Value
+    def cast(value)
+      return nil if value.blank? || !value.respond_to?(:to_money)
+      value.to_money
+    end
+
+    def serialize(money)
+      money.value
+    end
+  end
+
   module ActiveRecordHooks
     def self.included(base)
       base.extend(ClassMethods)
@@ -7,25 +18,7 @@ module MoneyColumn
     module ClassMethods
       def money_column(*columns)
         Array(columns).flatten.each do |name|
-          define_method(name) do
-            value = read_attribute(name)
-            value.blank? ? nil : Money.new(value)
-          end
-
-          define_method("#{name}_before_type_cast") do
-            send(name) && sprintf("%.2f", send(name))
-          end
-
-          define_method("#{name}=") do |value|
-            if value.blank? || !value.respond_to?(:to_money)
-              write_attribute(name, nil)
-              nil
-            else
-              money = value.to_money
-              write_attribute(name, money.value)
-              money
-            end
-          end
+          attribute name, MoneyColumn::Type.new
         end
       end
     end
