ed157158f9f9e4d8747eb7d875882395e860ee2d
Michael Elfassy <michael.elfassy@shopify.com>
2017-07-14 14:03:49 -0400

    load historic and non iso currencies

---
+++ spec/currency/loader_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe Money::Currency::Loader do
+
+  describe 'load_currencies' do
+    it 'loads the iso currency file' do
+      expect(subject.load_currencies['usd']['iso_code']).to eq('USD')
+      expect(subject.load_currencies['usd']['symbol']).to eq('$')
+      expect(subject.load_currencies['usd']['subunit_to_unit']).to eq(100)
+      expect(subject.load_currencies['usd']['smallest_denomination']).to eq(1)
+    end
+
+    it 'loads the non iso currency file' do
+      expect(subject.load_currencies['jep']['iso_code']).to eq('JEP')
+    end
+
+    it 'loads the historic iso currency file' do
+      expect(subject.load_currencies['eek']['iso_code']).to eq('EEK')
+    end
+  end
+end
