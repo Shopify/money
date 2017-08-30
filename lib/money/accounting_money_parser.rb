f4b177a5d6c533848b428be6da31209f5a72916c
Bart de Water <bart.dewater@shopify.com>
2017-08-30 13:59:45 -0400

    Stop instantiating parser objects that we don't use

--- lib/money/accounting_money_parser.rb
+++ lib/money/accounting_money_parser.rb
@@ -1,7 +1,11 @@
 class AccountingMoneyParser < MoneyParser
-  private
-  def extract_money(input)
+  class << self
+
+    private
+
+    def extract_money(input)
     # set () to mean negativity. ignore $
     super(input.gsub(/\(\$?(.*?)\)/, '-\1'))
+    end
   end
 end
--- lib/money/money_parser.rb
+++ lib/money/money_parser.rb
@@ -1,31 +1,28 @@
 class MoneyParser
-  # parse a amount from a string
-  def self.parse(input, currency = nil)
-    new.parse(input, currency)
-  end
+  class << self
+    def parse(input, currency = nil)
+      Money.new(extract_money(input.to_s), currency)
+    end
 
-  def parse(input, currency = nil)
-    Money.new(extract_money(input.to_s), currency)
-  end
+    private
+    def extract_money(input)
+      return 0 if input.to_s.empty?
 
-  private
-  def extract_money(input)
-    return 0 if input.to_s.empty?
+      amount = input.scan(/\-?[\d\.\,]+/).first
 
-    amount = input.scan(/\-?[\d\.\,]+/).first
+      return 0 if amount.nil?
 
-    return 0 if amount.nil?
+      # Convert 0.123 or 0,123 into what will be parsed as a decimal amount 0.12 or 0.13
+      amount.gsub!(/^(-)?(0[,.]\d\d)\d+$/, '\1\2')
 
-    # Convert 0.123 or 0,123 into what will be parsed as a decimal amount 0.12 or 0.13
-    amount.gsub!(/^(-)?(0[,.]\d\d)\d+$/, '\1\2')
+      segments = amount.scan(/^(.*?)(?:[\.\,](\d{1,2}))?$/).first
 
-    segments = amount.scan(/^(.*?)(?:[\.\,](\d{1,2}))?$/).first
+      return 0 if segments.empty?
 
-    return 0 if segments.empty?
+      amount   = segments[0].gsub(/[^-\d]/, '')
+      decimals = segments[1].to_s.ljust(2, '0')
 
-    amount   = segments[0].gsub(/[^-\d]/, '')
-    decimals = segments[1].to_s.ljust(2, '0')
-
-    "#{amount}.#{decimals}"
+      "#{amount}.#{decimals}"
+    end
   end
 end
