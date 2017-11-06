2eb0c1fb7ab5b146a25a30b4cc296561af82b8b4
Michael Elfassy <michael.elfassy@shopify.com>
2017-11-06 10:15:13 -0500

    money parser with currency support

--- lib/money/money_parser.rb
+++ lib/money/money_parser.rb
@@ -1,31 +1,65 @@
+# Parse an amount from a string
 class MoneyParser
-  # parse a amount from a string
+  MARKS = "\.,·’"
+  EXTRA_MARKS = "\s˙'"
+
   def self.parse(input, currency = nil)
     new.parse(input, currency)
   end
 
   def parse(input, currency = nil)
-    Money.new(extract_money(input.to_s), currency)
+    amount = extract_money(input.to_s, currency)
+    Money.new(amount, currency)
   end
 
   private
-  def extract_money(input)
-    return Money.zero if input.to_s.empty?
 
-    amount = input.scan(/\-?[\d\.\,]+/).first
+  def extract_money(input, currency)
+    return '0' if input.empty?
 
-    return Money.zero if amount.nil?
+    amount = input.scan(/(-?[\d#{MARKS}][\d#{MARKS}#{EXTRA_MARKS}]*)/).first
+    return '0' unless amount
+    amount = amount.first.tr(EXTRA_MARKS, '')
 
-    # Convert 0.123 or 0,123 into what will be parsed as a decimal amount 0.12 or 0.13
-    amount.gsub!(/^(-)?(0[,.]\d\d)\d+$/, '\1\2')
+    *other_marks, last_mark = amount.scan(/[#{MARKS}]/)
+    return amount unless last_mark
 
-    segments = amount.scan(/^(.*?)(?:[\.\,](\d{1,2}))?$/).first
+    *dollars, cents = amount.split(last_mark)
+    dollars = dollars.join.tr(MARKS, '')
 
-    return Money.zero if segments.empty?
+    if last_digits_decimals?(dollars, cents, last_mark, other_marks, currency)
+      "#{dollars}.#{cents}"
+    else
+      "#{dollars}#{cents}"
+    end
+  end
 
-    amount   = segments[0].gsub(/[^-\d]/, '')
-    decimals = segments[1].to_s.ljust(2, '0')
+  def last_digits_decimals?(first_digits, last_digits, last_mark, other_marks, currency)
+    # Thousands marks are always different from decimal marks
+    # Example: 1,234,456
+    other_marks.uniq!
+    if other_marks.size == 1
+      return other_marks.first != last_mark
+    end
 
-    "#{amount}.#{decimals}"
+    # Thousands always have more than 2 digits
+    # Example: 1,23 must be 1 dollar and 23 cents
+    if last_digits.size < 3
+      return true
+    end
+
+    # 0 before the final mark indicates last digits are decimals
+    # Example: 0,23
+    if first_digits.to_i.zero?
+      return true
+    end
+
+    # The last mark matches the one used by the provided currency to delimiter decimals
+    if currency
+      return Money::Helpers.value_to_currency(currency).decimal_mark == last_mark
+    end
+
+    # legacy support for 1.000
+    last_digits.size != 3
   end
 end
