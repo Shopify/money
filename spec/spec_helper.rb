2fa99609478681e5c531d288e90945b18933c673
Michael Elfassy <michaelelfassy@gmail.com>
2025-05-21 23:33:30 -0400

    reset loaded currencies
    in tests we sometimes need to reset the memoized currencies

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -54,6 +54,8 @@ RSpec.configure do |config|
     DatabaseCleaner.cleaning do
       example.run
     end
+  ensure
+    Money::Config.reset_current
   end
 end
 
@@ -72,7 +74,7 @@ RSpec::Matchers.define :quack_like do
 end
 
 
-def configure(default_currency: nil, legacy_json_format: nil, legacy_deprecations: nil, legacy_default_currency: nil)
+def configure(default_currency: nil, legacy_json_format: nil, legacy_deprecations: nil, legacy_default_currency: nil, experimental_crypto_currencies: nil)
   Money::Config.current = Money::Config.new.tap do |config|
     config.default_currency = default_currency if default_currency
     config.legacy_json_format! if legacy_json_format
@@ -80,6 +82,8 @@ def configure(default_currency: nil, legacy_json_format: nil, legacy_deprecation
     config.legacy_default_currency! if legacy_default_currency
     config.experimental_crypto_currencies! if experimental_crypto_currencies
   end
+  Money::Currency.reset_loaded_currencies if experimental_crypto_currencies == false
+
   yield
 ensure
   Money::Config.reset_current
